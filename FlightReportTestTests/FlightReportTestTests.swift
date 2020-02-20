cdf0ed50af7dd7ec2a7924cbdd0fc3ee3b288aab
Hokila <hokilajan@starlux-airlines.com>
2020-02-20 15:32:44 +0800

    add empty project

---
+++ FlightReportTestTests/FlightReportTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlightReportTestTests.swift
+//  FlightReportTestTests
+//
+//  Created by Hokila Jan 詹岳勳 on 2020/2/20.
+//  Copyright © 2020 JX. All rights reserved.
+//
+
+import XCTest
+@testable import FlightReportTest
+
+class FlightReportTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
